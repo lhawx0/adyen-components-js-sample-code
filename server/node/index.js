88d08c81e3e2a4f149d7801241ca71c3a35c94f6
lhawx0 <lhawx0@gmail.com>
2022-01-11 06:53:13 +0100

    test run ready

--- server/node/index.js
+++ server/node/index.js
@@ -26,6 +26,6 @@ module.exports = (() => {
     app.all('/sessions', (req, res) => sessions(res, req.body));
     app.all('/clientKeys', (req, res) => getClientKeys(res, req));
 
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 8000;
     app.listen(port, () => console.log(`Listening on localhost:${port}`));
 })();
